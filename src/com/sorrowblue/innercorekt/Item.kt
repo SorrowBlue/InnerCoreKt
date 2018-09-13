2e5bf820bd58a9ce38f717b8db75c786814e6e65
SorrowBlue <sorrowblue.dev@gmail.com>
2018-09-13 14:38:11 +0900

    updaate

--- src/com/sorrowblue/innercorekt/Item.kt
+++ src/com/sorrowblue/innercorekt/Item.kt
@@ -3,41 +3,89 @@ package com.sorrowblue.innercorekt
 external object Item {
 	
 	/**
+	 * 指定された名前、テクスチャ、およびパラメータを使用して、以前に登録された文字列IDにアイテムを作成します。
+	 */
+	fun createFoodItem(nameID: String, name: String, texture: Items.Texture, params: Items.Params = definedExternally)
+	
+	/**
 	 * 指定された名前、テクスチャ、およびパラメータを使用して、以前に登録された文字列IDに食用のアイテムを作成します。
 	 */
-	fun createFoodItem(stringID: String, name: String "オブジェクト名", texture: Texture, params: FoodParams)
+	fun createFoodItem(nameID: String, name: String, texture: Items.Texture, params: Items.Params = definedExternally)
+	
+	/**
+	 * 指定した名前、テクスチャ、およびパラメータを使用して、以前に登録された文字列IDに投擲アイテムを作成します。
+	 */
+	fun createThrowableItem(nameID: String, name: String, texture: Items.Texture, params: Items.Params = definedExternally) 
+	
+	/**
+	 * 指定された名前、テクスチャ、およびパラメータを使用して、以前に登録された文字列IDに防具アイテムを作成します。
+	 */
+	fun createArmorItem(nameID: String, name: String, texture: Items.Texture, params: Items.Armor)
+	
+	/**
+	 * アイテムのプロパティを設定する。
+	 */
+	fun describeItem(nameID: String, desc: Items.Desc)
+	
+	/**
+	 * カテゴリパラメータのアナログ
+	 */
+	fun setCategory(id: Int, category: Int)
+	
+	/**
+	 * アナログパラメータエンチャント
+	 */
+	fun setEnchantType(id: Int, type: Int, value: Int)
+	
+	/**
+	 * toolRenderパラメータのアナログ
+	 */
+	fun setToolRender(id: Int, enabled: Boolean)
+	
+	/**
+	 * maxDamageパラメータのアナログ
+	 */
+	fun setMaxDamage(id: Int, maxdamage: Int)
+	
+	/**
+	 * stackByDataパラメータのアナログ
+	 */
+	fun setStackedByData(id: Int, enabled: Boolean)
+	
+	/**
+	 * プロパティパラメータの類似
+	 */
+	fun setProperties(id: Int, Any?)
+	
+	/**
+	 * useAnimationパラメータのアナログ
+	 */
+	fun setUseAnimation(id: Int, animType: Int)
+	
+	/**
+	 * エンコードtrueの場合、次に、IDとデータとオブジェクトまたはブロックの名前を与えるitem.imya.nameまたはblock.imya.name形式に戻します
+	 */
+	fun getName(id: Int, data: Int, encode: Boolean = definedExternally): String
+	
+	/**
+	 * アイテムに最大のダメージを与えます。
+	 */
+	fun getMaxDamage(id: Int): Int
+	
+	/**
+	 * スタックのサイズを与える
+	 */
+	fun getMaxStack(id: Int): Int
+	
+	/**
+	 * 使用アニメーションを返します。
+	 */
+	fun getUseAnimation(id: Int): Int
+	
+	/**
+	 * このアイテムが存在するかどうかを返します。
+	 */
+	fun isValid(id: Int, data: Int): Boolean
+Item.isNativeItem（id） - このオブジェクトがMCPEオブジェクトであるかどうかを返します。modsではありません。
 
-パラメータには書式があります。デフォルト値を変更したくない場合は、書かないでください。
-
-{ 
-     isTech ： false 、// trueの場合、クリエイティブ
-     スタックにアイテムを追加しません： 64 、 // 
-     スタックサイズfood ： 1  // health restitution 
-}
-スローされたオブジェクトの作成
-Item.createThrowableItem（ "文字列ID"、 "オブジェクト名"、テクスチャ、パラメータ） - 指定した名前、テクスチャ、およびパラメータを使用して、以前に登録された文字列IDにスコープ付きオブジェクトを作成します。
-
-パラメータには書式があります。デフォルト値を変更したくない場合は、書かないでください。
-
-{ 
-     isTech ： false 、 // trueの場合、クリエイティブ
-     スタックにアイテムを追加しません： 64  // 1つのスタックに含まれるアイテムの数。最大64です
-。}
-スローされたオブジェクトの例。
-// TODO
-鎧を作る
-Item.createArmorItem（ "文字列ID"、 "サブジェクト名"、テクスチャ、パラメータ） - 指定された名前、テクスチャ、およびパラメータを使用して、以前に登録された文字列IDに装甲オブジェクトを作成します。
-パラメータには書式があります。デフォルト値を変更したくない場合は、書かないでください。
-
-{ 
-     IsTech ： falseに、 // trueの場合は、創造的にオブジェクトを追加しない
-     鎧： 1 、 規模に//追加装甲保護の量を
-     種類： < バインディング 値を> 、 //オブジェクトタイプは「ヘルメット」/「胸板」/ 「レギンス」/ "ブーツ" 
-     テクスチャ： < 必須 値> 、 //このアーマーのモデルのテクスチャー
-     耐久性： 1  //アーマーの強さ
-}
-	external data class DefaultParams(var isTech: Boolean, var stack: Int)
-	external data class FoodParams(var isTech: Boolean, var stack: Int, var health: Int)
-	external data class DefaultParams(var isTech: Boolean, var stack: Int)
-	external data class Texture(var name: String, var meta: Int)
 }
