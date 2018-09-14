6ae61fc410b66f4f4b591d3a1c74b764e29dd594
SorrowBlue <sorrowblue.dev@gmail.com>
2018-09-14 13:29:02 +0900

    [new] interface Coords etc
    interface Coords
    interface Coords.Relative
    interface Coords.Vec

--- src/com/sorrowblue/innercorekt/wrapper/Coords.kt
+++ src/com/sorrowblue/innercorekt/wrapper/Coords.kt
@@ -1,14 +1,37 @@
 package com.sorrowblue.innercorekt.wrapper
 
 /**
- * 座標管理クラス
+ * 座標管理
  */
-data class Coords(var x: Int, var y: Int, var z: Int, var side: Int = 0,
-				  var relative: Relative = Relative(x, y + 1, z),
-				  var vec: Vec = Vec(x.toDouble(), y.toDouble(), z.toDouble())) {
+interface Coords {
 	
-	data class Relative(var x: Int, var y: Int, var z: Int)
+	val x: Int
+	val y: Int
+	val z: Int
 	
-	data class Vec(var x: Double, var y: Double, var z: Double)
+	val side: Int get() = 0
 	
+	val relative: Relative get () = object : Relative {
+		override val x = 0
+		override val y = 0
+		override val z = 0
+	}
+	
+	val vec: Vec get() = object : Vec {
+		override val x = 0.0
+		override val y = 0.0
+		override val z = 0.0
+	}
+	
+	interface Relative {
+		val x: Int
+		val y: Int
+		val z: Int
+	}
+	
+	interface Relative {
+		val x: Double
+		val y: Double
+		val z: Double
+	}
 }
